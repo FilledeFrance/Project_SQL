9c50559642c44c0f7fe7d506c7bb07eefe2d8cd9
FilledeFrance <142030675+FilledeFrance@users.noreply.github.com>
2025-08-02 06:58:26 +0200

    Update Script_secondary_table.sql

--- Script_secondary_table.sql
+++ Script_secondary_table.sql
@@ -1,14 +1,17 @@
 CREATE TABLE t_sarka_motylova_project_sql_secondary_final AS
 SELECT 
-country, 
-"year", 
-gdp, 
-population, 
-gini
+ec.country, 
+ec.year, 
+ec.gdp, 
+ec.population, 
+ec.gini
 FROM economies AS ec
-WHERE country IN ('Luxembourg', 'Sweden', 'Ireland', 'San Marino', 'Portugal', 'Malta', 'Albania', 'Ukraine', 'Latvia', 'Slovakia', 'Bosni and Herzegovina','Cyprus', 'Serbia','Belgium','Monaco', 'Georgia', 'United Kingdom', 'Germany','Slovenia','Greece','Poland','Finland','France','Belarus','Netherlands','Spain','Italy','Lichtenstein','Estonia','Denmark','Switzerland','Hungary','Austria','Lithunia','Bulgaria','Croatia','Andorra')
+LEFT JOIN countries AS c
+	ON ec.country = c.country
+WHERE c.continent = 'Europe'
 	AND ec."year" > 2005
 	AND ec."year" <2019
+	AND ec.country != 'Czech Republic'
 
 
 SELECT *
@@ -19,4 +22,5 @@ ORDER BY
 
 	
 --pro smazani tabulky:	
+
 DROP TABLE t_sarka_motylova_project_sql_secondary_final 
